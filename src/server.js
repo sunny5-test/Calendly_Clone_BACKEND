3f2e3630a327e6bf9582457e40e2015665517b1e
sunny5-test <ok669312@gmail.com>
2026-04-16 22:02:10 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 
 // ─── Middleware ───────────────────────────────────────────
 app.use(cors({
-  origin: 'https://calendly-clone-front-end.vercel.app',
+  origin: 'https://calendly-clone-front-end.vercel.app/',
   credentials: true,
 }));
 app.use(express.json());
